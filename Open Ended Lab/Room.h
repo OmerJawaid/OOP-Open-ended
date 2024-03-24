2aaf7295eaca0cfa4f0a495037b68f2437a601ad
Ahmad Iftikhar <ahmadiftikhar200536@gmail.com>
2024-03-24 20:22:20 +0500

    class room updated

--- Open Ended Lab/Room.h
+++ Open Ended Lab/Room.h
@@ -1,21 +1,28 @@
 #pragma once
 #include<iostream>
+#include<vector>
+#include<algorithm>
+#include"Course.h"
 using namespace std;
 
 class Room
 {
 private:
-	string roomNumber;
-	int capacity;
+	int roomNumber;
+	vector<int>total_rooms = {417,418,419};
+	vector<int>rooms_available = {417,418,419};
+	int capacity, count = 0;
+	vector<int>total_labs = {401,402};
+	vector<int> available_labs = {401,402};
 public:
-	Room(string number, int cap) : roomNumber(number), capacity(cap) {
-		cout << "Room constructor has been called." << endl;
-	};
-	void checkAvailabitiy() {};
-	void bookRoom() {};
-	string getRoomNumber() const {
+    void displayRooms_available();
+	void assignRoom(Course* course);
+bool checkAvailabitiy(int room_number);
+void bookRoom();
+	int getRoomNumber() const {
 		return roomNumber;
 	}
 	int getcapacity() const { return capacity; }
 };
+void update_room(Course* course);
 
