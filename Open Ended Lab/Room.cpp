260159e294601614ef71bc931ebcf23c53400c6b
Ahmad Iftikhar <ahmadiftikhar200536@gmail.com>
2024-03-27 19:55:32 +0500

    room Modified

--- Open Ended Lab/Room.cpp
+++ Open Ended Lab/Room.cpp
@@ -1,58 +1,60 @@
 #include"Room.h"
+#include "Course.h" // Include Course.h for Course class
 
-void Room :: displayRooms_available()
-{
+Room::Room(string r_no,int cap) : roomNumber(r_no),capacity(cap) {
+    total_rooms = { "417","418","419" }; // Initialize total rooms
+    rooms_available = total_rooms; // Initially all rooms are available
+    total_labs = { "401","402" }; // Initialize total labs
+    available_labs = total_labs; // Initially all labs are available
+}
+
+void Room::displayRooms_available() {
     cout << "Rooms available: " << endl;
-    for (int i = 0; i < 3; i++)
-    {
+    for (int i = 0; i < rooms_available.size(); i++) {
         cout << rooms_available[i] << endl;
     }
 }
 
-void Room::assignRoom(Course* course)
-{
+void Room::assignRoom(Course* course) {
     bool check = false;
-    do
-    {
+    do {
         cout << "Enter room number: ";
         cin >> roomNumber;
-        check =  checkAvailabitiy(roomNumber);
-        if (check == true)
-        {
-            course->setAssignedRoom(this);
+        check = checkAvailabitiy(roomNumber);
+        if (check) {
+            course->setAssignedRoom(this); // Passing the Room pointer to Course
+            // Remove the assigned room from available rooms
+            rooms_available.erase(remove(rooms_available.begin(), rooms_available.end(), roomNumber), rooms_available.end());
         }
-        else
-        {
+        else {
             cout << "Room not available" << endl;
         }
-    } while (check == false);
+    } while (!check);
 }
-bool Room::checkAvailabitiy(int room_number) 
-{
-    for (int i = 0; i < 3; i++)
-    {
-        if (rooms_available[i] == room_number)
-        {
-            return true;
-        }
-    }
-    return false;
-}	
+    Room* assignedRoom; // Declare the variable "assignedRoom" in the Room class
 
-void Room :: update_room(Course* course)
-{
+    bool Room::checkAvailabitiy(string room_number) {
+        for (int i = 0; i < rooms_available.size(); i++) {
+            if (rooms_available[i] == room_number) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+void Room::update_room(Course* course) {
     int up_room;
-   cout<<"Enter room number that you want to update = ";
-   cin>>up_room;
-   auto it = find (total_rooms.begin(),total_rooms.end(),up_room);
-    if(it != total_rooms.end() )
-    {
-        course->setAssiignedRoom(up_room);
+    cout << "Enter room number that you want to update = ";
+    cin >> up_room;
+    auto it = find(rooms_available.begin(), rooms_available.end(), up_room);
+    if (it != rooms_available.end()) {
+        course->setAssignedRoom(this); // Passing the Room pointer to Course
     }
-    else
-    {
-        cout<<"Room Number dosen't exist . Try Again!" << endl;
+    else {
+        cout << "Room Number doesn't exist. Try Again!" << endl;
     }
-   
 }
-void Room::bookRoom() {}
+
+void Room::bookRoom() {
+    // Implementation for booking a room goes here if needed.
+}
--- Open Ended Lab/Room.h
+++ Open Ended Lab/Room.h
@@ -2,22 +2,29 @@
 #include<iostream>
 #include<vector>
 #include<algorithm>
-#include"Course.h"
 using namespace std;
 
-class Room
-{
+class Course;
+
+class Room {
 private:
-	string roomNumber;
-	int capacity;
+    string roomNumber;
+    vector<string> total_rooms;
+    vector<string> rooms_available;
+    int capacity; 
+	int count = 0;
+    vector<string> total_labs;
+    vector<string> available_labs;
+
 public:
-	Room(string number, int cap) : roomNumber(number), capacity(cap) {
-		cout << "Room constructor has been called." << endl;
-	};
-	void checkAvailabitiy() {};
-	void bookRoom() {};
-	string getRoomNumber() const {
-		return roomNumber;
-	}
-	int getcapacity() const { return capacity; }
+    Room(); Room(string r_no,int cap) : roomNumber(r_no),capacity(cap) {}
+    void displayRooms_available();
+    void assignRoom(Course* course);
+    bool checkAvailabitiy(string room_number);
+    void update_room(Course* course);
+    void bookRoom();
+    string getRoomNumber() const {
+        return roomNumber;
+    }
+    int getcapacity() const { return capacity; }
 };
